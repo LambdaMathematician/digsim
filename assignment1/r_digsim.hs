270d34c8c724f6a25f514af7fed58f349978da03
Nguyen <1517792051@mil>
2017-12-08 13:28:43 -0600

    made a rational version of assignment1

--- assignment1/r_digsim.hs
+++ assignment1/r_digsim.hs
@@ -1,15 +1,15 @@
 import Graphics.Gnuplot.Simple
 
-dampingCoefficient = 8.88 :: Double
-dt = 0.01 :: Double
-gravity = 9.88 :: Double
-mass = 1.0 :: Double
-springCoefficient = 39.47 :: Double
-tStop = 2.5 :: Double
-xIC = 0.0 :: Double
-xdIC = 0.0 :: Double
+dampingCoefficient = 8.88 :: Rational
+dt = 0.01 :: Rational
+gravity = 9.88 :: Rational
+mass = 1.0 :: Rational
+springCoefficient = 39.47 :: Rational
+tStop = 2.5 :: Rational
+xIC = 0.0 :: Rational
+xdIC = 0.0 :: Rational
 
-type State = (Double, Double, Double, Double)
+type State = (Rational, Rational, Rational, Rational)
 
 newstate :: State -> State
 newstate (t, x, xd, xdd) = (t', x', xd', xdd')
@@ -34,6 +34,10 @@ getTXdList = map getTXd
 makeXPlot = plotList [] $ getTXList meow
 makeXdPlot = plotList [] $ getTXdList meow
 
+mapTuple :: (a -> b) -> (a , a) -> (b, b)
+mapTuple f (a1, a2) = (f a1, f a2)
+
 main = do
     makeXPlot
     makeXdPlot
+    print $ map (mapTuple fromRational) $ getTXList meow
